04f0bcbd3dfdb64d610e63acee8a3200aa2730af
Dirk Wenzel <wenzel@cps-it.de>
2015-12-14 18:23:48 +0100

    [TASK] table configuration for performance, TypoScript, localized strings and sql edited.

--- Configuration/TCA/Overrides/tx_t3events_domain_model_performance.php
+++ Configuration/TCA/Overrides/tx_t3events_domain_model_performance.php
@@ -1,16 +1,38 @@
 <?php
+$ll = 'LLL:EXT:t3events_reservation/Resources/Private/Language/locallang_db.xlf:';
 
-$tmpColumns = array(
-	'participants' => array(
+$GLOBALS['TCA']['tx_t3events_domain_model_performance']['ctrl']['type'] = 'tx_extbase_type';
+
+$GLOBALS['TCA']['tx_t3events_domain_model_performance']['palettes']['paletteLessonRegistration'] = [
+	'showitem' => 'registration_begin,deadline',
+	'canNotCollapse' => TRUE
+];
+$extbaseType = 'Tx_T3eventsReservation_Schedule';
+$scheduleShowItems = '
+						--palette--;;paletteLessonDates,
+        				date_remarks,class_time,event_location,status,course,
+					--div--;' . $ll . 'label.tab.price,
+						price,free_of_charge,price_notice,
+					--div--;' . $ll . 'label.tab.registration,
+						--palette--;;paletteLessonRegistration,places,registration_remarks,document_based_registration,
+						registration_documents,external_registration,external_registration_link,
+					--div--;' . $ll . 'label.tab.participants,
+						participants,
+					--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
+					tx_extbase_type,hidden,starttime, endtime';
+$GLOBALS['TCA']['tx_t3events_domain_model_performance']['types'][$extbaseType]['showitem'] = $scheduleShowItems;
+
+$temporaryColumns = [
+	'participants' => [
 		'exclude' => 1,
-		'label' => 'LLL:EXT:t3events_course/Resources/Private/Language/locallang_db.xlf:tx_dakosyreservations_domain_model_reservation.participants',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.participants',
+		'config' => [
 			'type' => 'inline',
-			'foreign_table' => 'tx_dakosyreservations_domain_model_person',
-			'MM' => 'tx_dakosyreservations_lesson_participants_person_mm',
-			'appearance' => array(
+			'foreign_table' => 'tx_t3events_domain_model_person',
+			'MM' => 'tx_t3eventsreservation_performance_participants_person_mm',
+			'appearance' => [
 				'levelLinksPosition' => 'none',
-				'enabledControls' => array(
+				'enabledControls' => [
 					'info' => FALSE,
 					'new' => FALSE,
 					'dragdrop' => FALSE,
@@ -18,108 +40,139 @@ $tmpColumns = array(
 					'hide' => FALSE,
 					'delete' => FALSE,
 					'localize' => FALSE,
-				),
-			),
-		),
-	),
-	'deadline' => array(
+				],
+			],
+		],
+	],
+	'deadline' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.deadline',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.deadline',
+		'config' => [
 			'type' => 'input',
 			'size' => 7,
 			'eval' => 'date',
 			'checkbox' => 1,
 			'default' => time()
-		),
-	),
-	'registration_begin' => array(
+		],
+	],
+	'registration_begin' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.registration_begin',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.registration_begin',
+		'config' => [
 			'type' => 'input',
 			'size' => 7,
 			'eval' => 'date',
 			'checkbox' => 1
-		),
-	),
-	'price' => array(
+		],
+	],
+	'price' => [
 		'exclude' => 0,
 		'label' => 'LLL:EXT:t3events/Resources/Private/Language/locallang_db.xml:tx_t3events_domain_model_ticketclass.price',
-		'config' => array(
+		'config' => [
 			'type' => 'input',
 			'size' => 30,
 			'eval' => 'double2'
-		),
-	),
-	'free_of_charge' => array(
+		],
+	],
+	'places' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.free_of_charge',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.places',
+		'config' => [
+			'type' => 'input',
+			'size' => 4,
+			'eval' => 'int'
+		]
+	],
+	'free_of_charge' => [
+		'exclude' => 1,
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.free_of_charge',
+		'config' => [
 			'type' => 'check',
 			'default' => '0'
-		)
-	),
-	'registration_remarks' => array(
+		]
+	],
+	'registration_remarks' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.registration_remarks',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.registration_remarks',
+		'config' => [
 			'type' => 'text',
 			'cols' => 40,
 			'rows' => 5,
 			'eval' => 'trim'
-		)
-	),
-	'document_based_registration' => array(
+		]
+	],
+	'document_based_registration' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.document_based_registration',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.document_based_registration',
+		'config' => [
 			'type' => 'check',
 			'default' => '0'
-		)
-	),
-	'external_registration' => array(
+		]
+	],
+	'external_registration' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.external_registration',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.external_registration',
+		'config' => [
 			'type' => 'check',
 			'default' => '1'
-		)
-	),
-	'external_registration_link' => array(
+		]
+	],
+	'external_registration_link' => [
 		'exclude' => 1,
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.external_registration_link',
-		'config' => array(
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.external_registration_link',
+		'config' => [
 			'type' => 'input',
 			'size' => 30,
 			'eval' => 'trim'
-		)
-	),
-	'registration_documents' => array(
-		'label' => $ll . 'tx_dakosyreservations_domain_model_lesson.registration_documents',
-		'config' => array(
+		]
+	],
+	'registration_documents' => [
+		'label' => $ll . 'tx_t3eventsreservation_domain_model_performance.registration_documents',
+		'config' => [
 			'type' => 'group',
 			'internal_type' => 'db',
 			'allowed' => 'sys_file',
 			'MM' => 'sys_file_reference',
-			'MM_match_fields' => array(
+			'MM_match_fields' => [
 				'fieldname' => 'registration_documents'
-			),
+			],
 			'prepend_tname' => TRUE,
-			'appearance' => array(
+			'appearance' => [
 				'elementBrowserAllowed' => 'doc,dox,pdf',
 				'elementBrowserType' => 'file'
-			),
+			],
 			'max_size' => $GLOBALS['TYPO3_CONF_VARS']['BE']['maxFileSize'],
 			'show_thumbs' => '1',
 			'size' => '3',
 			'minitems' => '0',
 			'maxitems' => '200',
 			'autoSizeMax' => 40,
-		),
-	),
-);
+		],
+	],
+];
 
+// add type field if missing
+if (!isset($GLOBALS['TCA']['tx_t3events_domain_model_performance']['columns']['tx_extbase_type'])) {
+	$temporaryColumns['tx_extbase_type'] = [
+		'config' => [
+			'label' => $ll . 'label.tx_extbase_type',
+			'type' => 'select',
+			'items' => [
+				[$ll . 'label.tx_extbase_type.default', ''],
+				[$ll . 'label.tx_extbase_type.Tx_T3eventsReservation_Schedule', $extbaseType]
+			],
+		]
+	];
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+		'tx_t3events_domain_model_performance', 'tx_extbase_type', '', 'before:hidden');
+} else {
+	// add type item
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+		'tx_t3events_domain_model_performance',
+		'tx_extbase_type',
+		[$ll . 'label.tx_extbase_type.Tx_T3eventsReservation_Schedule', $extbaseType]
+	);
+}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
 	'tx_t3events_domain_model_performance',
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,45 +3,45 @@
 #
 CREATE TABLE tx_t3eventsreservation_domain_model_reservation (
 
-	uid INT(11) NOT NULL AUTO_INCREMENT,
-	pid INT(11) DEFAULT '0' NOT NULL,
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
 
-	status INT(11) DEFAULT '0' NOT NULL,
-	company INT(11) UNSIGNED DEFAULT '0',
-	contact INT(11) UNSIGNED DEFAULT '0',
-	participants INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	notifications INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	lesson INT(11) UNSIGNED DEFAULT '0',
-	privacy_statement_accepted TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-	offers_accepted TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-	contact_is_participant TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
+	status int(11) DEFAULT '0' NOT NULL,
+	company int(11) unsigned DEFAULT '0',
+	contact int(11) unsigned DEFAULT '0',
+	participants int(11) unsigned DEFAULT '0' NOT NULL,
+	notifications int(11) unsigned DEFAULT '0' NOT NULL,
+	lesson int(11) unsigned DEFAULT '0',
+	privacy_statement_accepted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	offers_accepted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	contact_is_participant tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
-	tstamp INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	crdate INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	cruser_id INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	deleted TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-	hidden TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-	starttime INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	endtime INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
 
-	t3ver_oid INT(11) DEFAULT '0' NOT NULL,
-	t3ver_id INT(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid INT(11) DEFAULT '0' NOT NULL,
-	t3ver_label VARCHAR(255) DEFAULT '' NOT NULL,
-	t3ver_state TINYINT(4) DEFAULT '0' NOT NULL,
-	t3ver_stage INT(11) DEFAULT '0' NOT NULL,
-	t3ver_count INT(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp INT(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id INT(11) DEFAULT '0' NOT NULL,
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
 
-	sys_language_uid INT(11) DEFAULT '0' NOT NULL,
-	l10n_parent INT(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource MEDIUMBLOB,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
 
 	PRIMARY KEY (uid),
 	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid, t3ver_wsid),
-	KEY language (l10n_parent, sys_language_uid)
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
 
 );
 
@@ -49,9 +49,9 @@ CREATE TABLE tx_t3eventsreservation_domain_model_reservation (
 # Table structure for table 'tx_t3events_domain_model_person'
 #
 CREATE TABLE tx_t3events_domain_model_person (
-	type INT(11) DEFAULT '0' NOT NULL,
-	reservation INT(11) DEFAULT '0' NOT NULL,
-	tx_extbase_type VARCHAR(255) DEFAULT '' NOT NULL
+	type int(11) DEFAULT '0' NOT NULL,
+	reservation int(11) DEFAULT '0' NOT NULL,
+	tx_extbase_type varchar(255) DEFAULT '' NOT NULL
 );
 
 #
@@ -65,19 +65,19 @@ CREATE TABLE tx_t3events_domain_model_performance (
 # Table structure for table 'tx_t3eventsreservation_reservation_participants_person_mm'
 #
 CREATE TABLE tx_t3eventsreservation_reservation_participants_person_mm (
-	uid_local INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	uid_foreign INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	sorting INT(11) UNSIGNED DEFAULT '0' NOT NULL,
-	sorting_foreign INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
 );
 
 #
-# Table structure for table 'tx_t3eventsreservation_lesson_participants_person_mm'
+# Table structure for table 'tx_t3eventsreservation_performance_participants_person_mm'
 #
-CREATE TABLE tx_t3eventreservation_lesson_participants_person_mm (
+CREATE TABLE tx_t3eventsreservation_performance_participants_person_mm (
 	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
 	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 	sorting int(11) unsigned DEFAULT '0' NOT NULL,
