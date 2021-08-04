0c2b179543742e8a2bad7c646f4555f41e3d2813
Santiago Nunez Negrillo <s.nuneznegrillo@gmail.com>
2021-08-04 18:12:42 +0200

    [TASK] Adjust TCA configuration for TYPO3 v10

--- Configuration/TCA/Overrides/tx_t3events_domain_model_performance.php
+++ Configuration/TCA/Overrides/tx_t3events_domain_model_performance.php
@@ -131,23 +131,23 @@ $temporaryColumns = [
                     'types' => [
                         '0' => [
                             'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
+                            --palette--;;imageoverlayPalette,
+                            --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
                             'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
+                            --palette--;;imageoverlayPalette,
+                            --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
                             'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
+                            --palette--;;imageoverlayPalette,
+                            --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
                             'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
+                            --palette--;;imageoverlayPalette,
+                            --palette--;;filePalette'
                         ]
                     ],
                 ],
